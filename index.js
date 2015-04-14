8a065a2926a3f8d8e646ea36b915d6498f78fcfe
Bennett Lin <bennettslin@gmail.com>
2015-04-14 15:35:57 -0700

    Once again...

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ passport.use(new FacebookStrategy({
 
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: BASE_URL + '/auth/callback/facebook'
+  callbackURL: BASE_URL + '/facebook/callback'
 
 }, function(accessToken, refreshToken, profile, done) {
 
